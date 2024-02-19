41085decfd98992ea26801b5918f7a24478389e7
Tom Peterka <tpeterka@mcs.anl.gov>
2024-02-19 09:54:00 -0600

    Debugging

--- src/prod-con.hpp
+++ src/prod-con.hpp
@@ -4,28 +4,18 @@
 #include    <cassert>
 #include    <thread>
 #include    <mutex>
+#include    <functional>
 
 // diy
 #include    <diy/mpi/communicator.hpp>
 #include    <diy/decomposition.hpp>
 #include    <diy/assigner.hpp>
 
+#include    "opts.h"
+
 #include    <fmt/format.h>
 
 #include    <hdf5.h>
-
-#include    "opts.h"
-
-// #ifdef LOWFIVE_PATH
-// 
-// #include    <lowfive/vol-metadata.hpp>
-// #include    <lowfive/vol-dist-metadata.hpp>
-// #include    <lowfive/log.hpp>
-// #include    <lowfive/H5VOLProperty.hpp>
-// 
-// namespace l5        = LowFive;
-// 
-// #endif
 
 // moab
 #include    "iMesh.h"
@@ -45,3 +35,27 @@ using Bounds        = diy::DiscreteBounds;
 
 using namespace moab;
 
+void hex_mesh_gen(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        ParallelComm *mbpc, diy::RegularDecomposer<Bounds>& decomp, diy::RoundRobinAssigner& assign);
+void tet_mesh_gen(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        ParallelComm *mbpc, diy::RegularDecomposer<Bounds>& decomp, diy::RoundRobinAssigner& assign);
+void create_hexes_and_verts(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        diy::RegularDecomposer<Bounds>& decomp, diy::RoundRobinAssigner& assign, ParallelComm* mbpc);
+void create_tets_and_verts(int *mesh_size, Interface *mbint, EntityHandle *mesh_set,
+        diy::RegularDecomposer<Bounds>& decomp, diy::RoundRobinAssigner& assign, ParallelComm* mbpc);
+void resolve_and_exchange(Interface *mbint, EntityHandle *mesh_set, ParallelComm *mbpc);
+
+double PhysField(double x, double y, double z, double factor);
+
+void PutElementField(Interface *mbi, EntityHandle eh, const char *tagname, double factor);
+
+void GetElementField(Interface *mbi, EntityHandle eh, const char *tagname, double factor, MPI_Comm comm, bool debug);
+
+void PutVertexField(Interface *mbi, EntityHandle eh, const char *tagname, double factor);
+
+void GetVertexField(Interface *mbi, EntityHandle eh, const char *tagname, double factor, MPI_Comm comm, bool debug);
+
+void PrintMeshStats(Interface *mbint, EntityHandle *mesh_set, ParallelComm *mbpc);
+
+void PrepMesh(int src_type, int src_size, int slab, Interface* mbi, ParallelComm* pc, EntityHandle root, double factor,
+        bool debug);
