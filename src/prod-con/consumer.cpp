e4e2c830e6c7e89a60ca1bacf5c6866c3d0fecde
Orcun Yildiz <oyildiz@beboplogin2.lcrc.anl.gov>
2024-05-09 14:50:43 -0500

    adding the missing moab comm creation to consumer

--- src/prod-con/consumer.cpp
+++ src/prod-con/consumer.cpp
@@ -25,6 +25,7 @@ int main(int argc, char**argv)
 
     // initialize moab
     Interface*                      mbi = new Core();                       // moab interface
+    ParallelComm*                   pc  = new ParallelComm(mbi, local);     // moab communicator
     EntityHandle                    root;
     ErrorCode                       rval;
     rval = mbi->create_meshset(MESHSET_SET, root); ERR(rval);
