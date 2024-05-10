6f4c804b62a61c3a34bfaafd226008748139a35a
Orcun Yildiz <oyildiz@beboplogin1.lcrc.anl.gov>
2024-05-10 15:48:57 -0500

    fixing the actions script for remap workflow

--- src/remap/remap_actions.py
+++ src/remap/remap_actions.py
@@ -5,14 +5,14 @@ def mpas_callback(vol, rank):
     # define a callback to broadcast/receive files before a file open
     def mpas_bfo_cb(name):
         print("prod_callback mpas_bfo_cb: name =", name)
-        if name == "outfile.h5m":
+        if name == "mpas_outfile.h5m":
             vol.broadcast_files()
 
     # define a callback to serve files after a file close
     def mpas_afc_cb(name):
         print("prod_callback mpas_afc_cb: name =", name)
         global nmpas_afc
-        if name != "outfile.h5m":
+        if name != "mpas_outfile.h5m":
             return
 
         if rank == 0:
@@ -20,10 +20,16 @@ def mpas_callback(vol, rank):
                 if vol.is_passthru(name, "*") == False:
                     vol.serve_all()
                     vol.serve_all()
+                else:
+                    print("signaling consumer at mpas")
+                    vol.serve_all(True, False) # signaling consumer that file is ready in passthru mode. TODO: Will be handled by Wilkins.
         else:
             if vol.is_passthru(name, "*") == False:
                 vol.serve_all()
                 vol.serve_all()
+            else:
+                print("signaling consumer at mpas")
+                vol.serve_all(True, False) # signaling consumer that file is ready in passthru mode
 
         nmpas_afc += 1
 
@@ -44,14 +50,14 @@ def roms_callback(vol, rank):
     # define a callback to broadcast/receive files before a file open
     def roms_bfo_cb(name):
         print("prod_callback roms_bfo_cb: name =", name)
-        if name == "outfile.h5m":
+        if name == "roms_outfile.h5m":
             vol.broadcast_files()
 
     # define a callback to serve files after a file close
     def roms_afc_cb(name):
         print("prod_callback roms_afc_cb: name =", name)
         global nroms_afc
-        if name != "outfile.h5m":
+        if name != "roms_outfile.h5m":
             return
 
         if rank == 0:
